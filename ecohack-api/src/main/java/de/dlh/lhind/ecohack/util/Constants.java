46ff302460386df0f5cb3dbfa25e6fcbdd888b95
Rezar Dalipaj <rezar.dalipaj@fti.edu.al>
2023-05-11 15:36:55 +0200

    Cleaned exceptionHandler class

--- ecohack-api/src/main/java/de/dlh/lhind/ecohack/util/Constants.java
+++ ecohack-api/src/main/java/de/dlh/lhind/ecohack/util/Constants.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Constants {
+
     public static final String NOT_FOUND_MESSAGE = "Not found";
     public static final String UNAUTHORIZED_MESSAGE = "Unauthorized!";
     public static final String REFRESH_PATH = "/refresh/token";
